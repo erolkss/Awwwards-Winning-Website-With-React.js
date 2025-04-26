9d582a8f2dce55cad7362491990fe55706d40091
Lucas Lima <lucaserolima@gmail.com>
2025-04-25 23:45:27 -0400

    feat: add Contact section with image clip paths and animated call to action

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
             <About/>
             <Features/>
             <Story/>
+            <Contact/>
         </main>
     )
 }
