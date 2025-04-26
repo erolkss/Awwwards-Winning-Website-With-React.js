c3933c754f61636bcc8cb217b76bef2e05f328b5
Lucas Lima <lucaserolima@gmail.com>
2025-04-25 23:46:12 -0400

    feat: add responsive footer with social links and privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
 import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
             <Features/>
             <Story/>
             <Contact/>
+            <Footer/>
         </main>
     )
 }
